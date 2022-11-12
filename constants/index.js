5dde7bb9c7a072f49ea5b94ef6b39171de98fa1b
Dmitrii Kozhekhov <dmitrykozhekhov@yandex.ru>
2022-11-12 16:41:20 +0300

    backend: auth, register ver_1.5

--- constants/index.js
+++ constants/index.js
@@ -1,9 +1,7 @@
 const NOT_FOUND = 'NotFound';
 const CAST_ERROR = 'CastError';
-const Err400 = 400;
-const Err404 = 404;
-const Err500 = 500;
+const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)/;
 const JWT_SECRET = '5066e19ca0f3f2521d7c7ef07d99c7307c5533cea846d26f4b9408932f1fc99f';
 module.exports = {
-  NOT_FOUND, CAST_ERROR, Err400, Err404, Err500, JWT_SECRET,
+  NOT_FOUND, CAST_ERROR, JWT_SECRET, regex,
 };
