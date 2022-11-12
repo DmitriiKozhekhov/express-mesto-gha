4c1a6ee3dd2e2a5f52347d388060b56859cd12a9
Dmitrii Kozhekhov <dmitrykozhekhov@yandex.ru>
2022-11-12 15:09:40 +0300

    backend: auth, register ver_1.2

--- routes/users.js
+++ routes/users.js
@@ -12,10 +12,10 @@ const {
 
 router.use(auth);
 router.get('/', getUsers);
-router.get('/:userId', validationOfUserId, getUser);
 router.get('/me', getUser);
 router.post('/', createUser);
 router.patch('/me', validationOfUser, editUser);
 router.patch('/me/avatar', validationOfAvatar, editAvatar);
+router.get('/:userId', validationOfUserId, getUser);
 
 module.exports = router;
